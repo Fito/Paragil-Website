168eeb57d963b4fc8ad12b8aa6c4c3f6d4292f27
Fito <fito@paragil.com>
2012-08-07 20:11:14 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebSite::Application.config.secret_token = '3727e9249e09e07efceffbc04d0c6c87ded175c86d77a5f005ba9e1fff17a856ef5acd6314449c044cc7b88c312f52a571b19bea455aea7da246b4ca9f3f1e58'
